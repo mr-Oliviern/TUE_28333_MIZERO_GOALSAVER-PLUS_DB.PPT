07eb07eeebac721be210ce344afc5afc45b54293
Mr-Olivier <mizeolivier@gmail.com>
2025-12-20 19:40:02 +0200

    Update validation_queries.sql

--- Database/Scripts/validation_queries.sql
+++ Database/Scripts/validation_queries.sql
@@ -1,1 +1,14 @@
-
+SELECT * FROM project.loan WHERE status = 'ACTIVE';
+SELECT * FROM project.member;
+SELECT
+    m.member_id,
+    m.first_name,
+    a.account_no,
+    t.transaction_no,
+    t.amount
+FROM member m
+JOIN account a
+    ON m.member_id = a.member_id
+JOIN account_transaction t
+    ON a.account_id = t.account_id
+ORDER BY m.member_id;
