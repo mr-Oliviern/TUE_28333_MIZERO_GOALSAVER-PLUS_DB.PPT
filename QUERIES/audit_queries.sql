e1eb78eea8684064c6d13a74dab919b6136ab157
Mr-Olivier <mizeolivier@gmail.com>
2025-12-21 13:09:35 +0200

    Update audit_queries.sql

--- QUERIES/audit_queries.sql
+++ QUERIES/audit_queries.sql
@@ -2,19 +2,7 @@
 SELECT * FROM audit_log
 ORDER BY action_date DESC;
 
--- 2. Failed actions only
-SELECT user_name, table_name, action_type, record_id, error_message, action_date
-FROM audit_log
-WHERE status = 'FAILED'
-ORDER BY action_date DESC;
-
--- 3. Successful actions only
-SELECT user_name, table_name, action_type, record_id, action_date
-FROM audit_log
-WHERE status = 'SUCCESS'
-ORDER BY action_date DESC;
-
--- 4. Loan validation audit
+-- 2. Loan validation audit
 SELECT *
 FROM audit_log
 WHERE table_name = 'LOAN'
