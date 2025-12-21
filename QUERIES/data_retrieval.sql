10811e8db915d0efbd77aa317cc2095f6612820b
Mr-Olivier <mizeolivier@gmail.com>
2025-12-21 13:07:05 +0200

    Update data_retrieval.sql

--- QUERIES/data_retrieval.sql
+++ QUERIES/data_retrieval.sql
@@ -1,20 +1,9 @@
--- 1. Get member account details
-SELECT a.account_id, a.account_no, a.account_type, a.account_amount, m.member_id, m.full_name
-FROM account a
-JOIN member m ON a.member_id = m.member_id;
-
--- 2. Transaction history for a given account
-SELECT transaction_id, transaction_type, amount, transaction_date
-FROM account_transaction
-WHERE account_id = :p_account_id
-ORDER BY transaction_date DESC;
-
--- 3. Loan details per account
+--1 Loan details per account
 SELECT loan_id, loan_amount, loan_date, loan_status
 FROM loan
 WHERE account_id = :p_account_id;
 
--- 4. Public holidays list
+-- 2. Public holidays list
 SELECT * FROM public_holidays
 ORDER BY holiday_date;
 
