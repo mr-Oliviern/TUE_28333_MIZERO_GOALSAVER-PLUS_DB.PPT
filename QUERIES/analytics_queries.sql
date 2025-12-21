12582b3f7046554eb8e31ed4326c0bb1b44a1790
Mr-Olivier <mizeolivier@gmail.com>
2025-12-21 11:01:53 +0200

    Update analytics_queries.sql

--- QUERIES/analytics_queries.sql
+++ QUERIES/analytics_queries.sql
@@ -1,1 +1,23 @@
+-- 1. Accounts below minimum savings
+SELECT account_id, member_id, account_amount
+FROM account
+WHERE account_amount < 1000;
+
+-- 2. Loan requests pending approval
+SELECT loan_id, account_id, loan_amount, loan_date
+FROM loan
+WHERE loan_status = 'PENDING';
+
+-- 3. Total deposits per account
+SELECT account_id, SUM(amount) AS total_deposit
+FROM account_transaction
+WHERE transaction_type = 'DEPOSIT'
+GROUP BY account_id;
+
+-- 4. Number of failed transactions per day
+SELECT TRUNC(action_date) AS txn_date, COUNT(*) AS failed_count
+FROM audit_log
+WHERE status = 'FAILED'
+GROUP BY TRUNC(action_date)
+ORDER BY txn_date;
 
